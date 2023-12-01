c686d27dd1a9624c5d17eb77dbe6abbb6782d19f
GUNDETIRAKSHITHA <97968070+GUNDETIRAKSHITHA@users.noreply.github.com>
2023-12-02 00:47:40 +0530

    Update Q2-receiver.py

--- Q2-receiver.py
+++ Q2-receiver.py
@@ -1,12 +1,26 @@
+
 import socket
 
-host = 'local host'
+host = 'localhost'  
 port = 5000
-s = socket.socket(socket.AF_INET,
-				socket.SOCK_STREAM)
-s.connect(('127.0.0.1', port))
-msg = s.recv(1024)
-while msg:
-	print('Received:' + msg.decode())
-	msg = s.recv(1024)
-s.close()
+
+try:
+    # Create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to the server
+    s.connect(('127.0.0.1', port))
+
+    # Receive and print messages
+    msg = s.recv(1024)
+    while msg:
+        print('Received: ' + msg.decode())
+        msg = s.recv(1024)
+
+except socket.error as e:
+    print(f"Socket error: {e}")
+
+finally:
+    # Close the socket in a finally block to ensure it always gets closed
+    if s:
+        s.close()
