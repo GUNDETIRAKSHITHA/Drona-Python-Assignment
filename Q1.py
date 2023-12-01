4963b4bae497a0ea962b240fcabd3eebb715e957
GUNDETIRAKSHITHA <97968070+GUNDETIRAKSHITHA@users.noreply.github.com>
2023-12-02 00:55:25 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,63 +1,37 @@
 import turtle
 
-# Define screen dimensions and background color
+# Set up the Turtle screen
 screen = turtle.Screen()
-screen.setup(width=800, height=600)
-screen.bgcolor("skyblue")
+screen.title("Drone Movement Simulator")
 
-# Create a turtle object for the drone
+# Create a drone turtle
 drone = turtle.Turtle()
-drone.speed(0)
-drone.penup()
+drone.shape("triangle")
+drone.color("blue")
+drone.speed(2)
 
-# Define color and size of drone body
-body_color = "blue"
-body_radius = 15
+# Function to move the drone forward
+def move_forward():
+    drone.forward(50)
 
-# Draw the drone body as a circle
-drone.fillcolor(body_color)
-drone.begin_fill()
-drone.circle(body_radius)
-drone.end_fill()
+# Function to move the drone backward
+def move_backward():
+    drone.backward(50)
 
-# Draw the propeller as a smaller circle on top
-propeller_color = "yellow"
-propeller_radius = 5
-drone.penup()
-drone.goto(0, body_radius + propeller_radius)
-drone.pendown()
-drone.fillcolor(propeller_color)
-drone.begin_fill()
-drone.circle(propeller_radius)
-drone.end_fill()
+# Function to move the drone left
+def move_left():
+    drone.left(90)
 
-# Define functions to move the drone
-def move_forward(distance):
-    drone.forward(distance)
+# Function to move the drone right
+def move_right():
+    drone.right(90)
 
-def move_backward(distance):
-    drone.backward(distance)
-
-def move_left(angle):
-    drone.left(angle)
-
-def move_right(angle):
-    drone.right(angle)
-
-# Bind keyboard keys to movement functions
-screen.onkey(lambda: move_forward(5), "Up")
-screen.onkey(lambda: move_backward(5), "Down")
-screen.onkey(lambda: move_left(5), "Left")
-screen.onkey(lambda: move_right(5), "Right")
-
-# Start listening for keyboard events
+# Keyboard bindings
 screen.listen()
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
 
-# Example usage of movement functions
-move_forward(50)
-move_right(90)
-move_backward(30)
-move_left(45)
-
-# Keep the window open
-turtle.done()
+# Close the window on click
+screen.exitonclick()
