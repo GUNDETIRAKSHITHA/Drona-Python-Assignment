ef825005f6ccf00d0ede4db0233d5a842c584119
GUNDETIRAKSHITHA <97968070+GUNDETIRAKSHITHA@users.noreply.github.com>
2023-12-02 00:48:14 +0530

    Update Q2-sender.py

--- Q2-sender.py
+++ Q2-sender.py
@@ -1,14 +1,37 @@
+
+
 import socket
-host = 'local host'
+
+host = 'localhost' 
 port = 5000
-s = socket.socket(socket.AF_INET, 
-				socket.SOCK_STREAM)
-s.bind(('', port))
-s.listen(1)
-c, addr = s.accept()
-print("CONNECTION FROM:", str(addr))
-c.send(b"HELLO, How are you ? \
-	Welcome to the World")
-msg = "Bye.............."
-c.send(msg.encode())
-c.close()
+
+try:
+    # Create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to a specific address and port
+    s.bind(('', port))
+
+    # Listen for incoming connections
+    s.listen(1)
+
+    # Accept a connection
+    c, addr = s.accept()
+    print("CONNECTION FROM:", str(addr))
+
+    # Send a welcome message
+    c.send(b"HELLO, How are you ? Welcome to the World")
+
+    # Send a farewell message
+    msg = "Bye.............."
+    c.send(msg.encode())
+
+except socket.error as e:
+    print(f"Socket error: {e}")
+
+finally:
+    # Close the connection and the socket in a finally block
+    if 'c' in locals():
+        c.close()
+    if 's' in locals():
+        s.close()
